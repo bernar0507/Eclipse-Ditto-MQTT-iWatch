b8b7b5d8042be8f9447f20c9c31ef2768aae5321
Bernardo Augusto <46348014+bernar0507@users.noreply.github.com>
2023-04-02 17:17:18 +0100

    Update send_data_iwatch.py

--- iwatch/send_data_iwatch.py
+++ iwatch/send_data_iwatch.py
@@ -5,8 +5,8 @@ import time
 import os
 
 # Replace with your own values
-#MQTT_BROKER_ADDRESS = "172.18.0.10" #"IP_ADDRESS_MQTT"
-MQTT_BROKER_ADDRESS = os.environ.get("MQTT_BROKER_ADDRESS", "localhost")
+MQTT_BROKER_ADDRESS = "172.18.0.10" #"IP_ADDRESS_MQTT"
+#MQTT_BROKER_ADDRESS = os.environ.get("MQTT_BROKER_ADDRESS", "localhost")
 MQTT_BROKER_PORT = 1883
 THING_ID = "org.Iotp2c:iwatch"
 MQTT_TOPIC = f"{THING_ID}/things/twin/commands/modify"
